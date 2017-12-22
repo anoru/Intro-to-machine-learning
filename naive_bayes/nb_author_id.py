b15f759251e5a55cf8492d524caeb76ccea6550c
anoru <anouar6in@gmail.com>
2017-12-22 16:52:31 +0000

    Solve naive bayes mini project
    Adding code to the starter code to solve naive bayes mini project
    in udacity Intro to machine learning course.
    At the ned of the file you find the output I got

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -23,3 +23,44 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 #########################################################
+def NBAccuracy(features_train, labels_train, features_test, labels_test):
+    """ compute the accuracy of your Naive Bayes classifier """
+    ### import the sklearn module for GaussianNB
+    from sklearn.naive_bayes import GaussianNB
+
+    ### create classifier
+    clf = GaussianNB()
+
+    ### Calculate the Time spent to train our algorithm
+    t0 = time()
+    ### fit the classifier on the training features and labels
+    clf.fit(features_train, labels_train)
+    print "Training time:", round(time()-t0, 3), "s"
+
+    ### Calculate the Time spent in the prediction
+    t0 = time()
+    ### use the trained classifier to predict labels for the test features
+    pred = clf.predict(features_test)
+
+    print "Prediction time:", round(time()-t0, 3), "s"
+
+    ### calculate and return the accuracy on the test data
+    from sklearn.metrics import accuracy_score
+    accuracy = accuracy_score(pred, labels_test)
+    
+    ### Another way
+    ### accuracy = clf.score(features_test, labels_test)
+    return accuracy
+
+print NBAccuracy(features_train, labels_train, features_test, labels_test)
+#########################################################
+
+### Your exact results for time may vary, 
+### but we found that predicting with this particular 
+### setup takes about 30x less time than training.
+
+#no. of Chris training emails: 7936
+#no. of Sara training emails: 7884
+#Training time: 1.299 s
+#Prediction time: 0.199 s
+#0.973265073948
