24cc11ec269b892638945fd229d9d4605c890888
anoru <anouar6in@gmail.com>
2018-01-02 09:43:19 +0000

    feat: use k nearest neighbors in choose your own mini project
    Use k nearest neighbors in choose your own algorithm mini project

---
+++ choose_your_own/k_nearest_neighbors.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python
+
+import matplotlib.pyplot as plt
+from prep_terrain_data import makeTerrainData
+from class_vis import prettyPicture
+from time import time
+
+features_train, labels_train, features_test, labels_test = makeTerrainData()
+
+
+### the training data (features_train, labels_train) have both "fast" and "slow"
+### points mixed together--separate them so we can give them different colors
+### in the scatterplot and identify them visually
+grade_fast = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==0]
+bumpy_fast = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==0]
+grade_slow = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==1]
+bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==1]
+
+
+#### initial visualization
+plt.xlim(0.0, 1.0)
+plt.ylim(0.0, 1.0)
+plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
+plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
+plt.legend()
+plt.xlabel("bumpiness")
+plt.ylabel("grade")
+plt.show()
+################################################################################
+
+
+### Name your classifier object clf if you want the 
+### visualization code (prettyPicture) to show you the decision boundary
+def NBAccuracy(features_train, labels_train, features_test, labels_test):
+    """ compute the accuracy of your Naive Bayes classifier """
+    ### import the sklearn module for k nearest nighbors
+    from sklearn.neighbors import KNeighborsClassifier
+
+    ### create classifier tune it with n_neighbors=20, weights='distance'
+    clf = KNeighborsClassifier(n_neighbors=20, weights='uniform')
+
+    ### Calculate the Time spent to train our algorithm
+    t0 = time()
+    ### fit the classifier on the training features and labels
+    clf.fit(features_train, labels_train)
+    print "Training time:", round(time()-t0, 3), "s"
+
+    ### Calculate the Time spent in the prediction
+    t0 = time()
+    ### use the trained classifier to predict labels for the test features
+    pred = clf.predict(features_test)
+
+    print "Prediction time:", round(time()-t0, 3), "s"
+
+    #print "Prediction for element #10:", pred[10]
+    #print "Prediction for element #26:", pred[26]
+    #print "Prediction for element #50:", pred[50]
+    #print "We could predict ", (sum(i == 1 for i in pred)),"in ", len(features_test),"test events bilong to Chris"
+
+    ### calculate and return the accuracy on the test data
+    from sklearn.metrics import accuracy_score
+    accuracy = accuracy_score(pred, labels_test)
+    
+    ### Another way
+    ### accuracy = clf.score(features_test, labels_test)
+    return accuracy
+
+print NBAccuracy(features_train, labels_train, features_test, labels_test)
+
+try:
+    prettyPicture(clf, features_test, labels_test)
+except NameError:
+    pass
+
+
+## No Tuning
+#Training time: 0.001 s
+#Prediction time: 0.002 s
+#0.92
+
+## n_neighbors=20
+#Training time: 0.001 s
+#Prediction time: 0.002 s
+#0.936
+
+## n_neighbors=20, weights='distance'
+#Training time: 0.001 s
+#Prediction time: 0.002 s
+#0.936
